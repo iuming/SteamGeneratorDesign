78ca6eac191ef3990124a6d4f478ac5e1178bba1
iuming <6ming@hrbeu.edu.cn>
2021-02-21 14:50:26 +0800

    add code & update manual

---
+++ code/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+
+int main(int argc , char** _argv){
+    cout<< "Hi"<<endl;
+    return 0;
+}
