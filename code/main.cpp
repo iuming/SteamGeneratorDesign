e5f1c4f00c112581441736e1b8f6b1bbc4739f8e
iuming <6ming@hrbeu.edu.cn>
2021-03-03 16:24:58 +0800

    添加主函数代码

--- code/main.cpp
+++ code/main.cpp
@@ -5,10 +5,51 @@
 #include <iomanip>
 #include <fstream>
 
+#include "CalculationOfAccelerationResistanceOfSecondaryCircuitWaterRisingSpace.h"
+#include "CalculationOfFrictionalResistanceInUTube.h"
+#include "CalculationOfLocalResistanceOfPrimaryCircuitWater.h"
+#include "CalculationOfResistanceOfFlowDistributionOrificeInSecondaryWaterRisingSpace.h"
+#include "CalculationOfResistanceOfSecondaryCircuitWaterVaporSeparator.h"
+#include "CalculationOfTheFrictionalResistanceOfTheSecondaryCircuitWaterRisingSpace.h"
+#include "CalculationOfTheLocalResistanceOfTheSecondaryCircuitWaterRisingSpace.h"
+#include "CalculationOfTheResistanceOfTheWaterBendInTheSecondaryCircuitWaterRiseSpace.h"
+#include "CalculationOfTheSpaceResistanceOfTheSecondaryCircuitWaterDrop.h"
+#include "CalculationOfTheTotalResistanceOfTheSecondaryCircuitWaterRisingSpace.h"
+#include "CalculationOfTotalResistanceOfPrimaryCircuitWater.h"
+#include "CalculationOfTotalResistanceOfSecondaryCircuitWaterCirculation.h"
+#include "GivenCondition.h"
+#include "HeatTransferCalculation.h"
+#include "HeatTransferTube.h"
+#include "LowerCylinder.h"
+#include "MainPipeInnerDiameter.h"
+#include "MotionHeadCalculation.h"
+#include "PreheatingSectionHeightCalculation.h"
+#include "SphericalBottomHead.h"
+#include "TubeBundleStructure.h"
+#include "UpperCylinder.h"
+
+
 using namespace std;
 
 
 int main(int argc , char** _argv){
-    cout<< "Hi"<<endl;
+
+    ios::sync_with_stdio(false);
+    
+    thermalEquilibrium();
+    heatTransfer();
+    tubeBundle();
+    mainPipeInnerDiameter();
+    primaryCircuitWaterResistance();
+    waterCirculationResistanceInTheSecondaryCircuit();
+    resistanceOfWaterSeparator();
+    totalCirculationResistance();
+    motionHead();
+    heatTransferTube();
+    lowerCylinder();
+    upperCylinder();
+    sphericalLowerHead();
+    tubeSheet();
+    
     return 0;
 }
